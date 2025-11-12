3ce0c9fc0571fd31d5b2df57e5ff22e47c11e11b
rarathore02 <rashikarathore.rr02@gmail.com>
2025-11-12 10:25:45 +0530

    Create q3.cpp

---
+++ q3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+bool isBalanced(string exp) {
+    stack<char> s;
+    for (char c : exp) {
+        if (c == '(' || c == '{' || c == '[')
+            s.push(c);
+        else if (c == ')' || c == '}' || c == ']') {
+            if (s.empty()) return false;
+            char top = s.top();
+            s.pop();
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '['))
+                return false;
+        }
+    }
+    return s.empty();
+}
+
+int main() {
+    string exp = "{[()]}";
+    if (isBalanced(exp))
+        cout << "Balanced Expression\n";
+    else
+        cout << "Not Balanced\n";
+}
